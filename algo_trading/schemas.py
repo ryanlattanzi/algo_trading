94eba64af8d29c40b8f3bd40f9e8aaddf75eedab
Ryan Lattanzi <45903952+ryanlattanzi@users.noreply.github.com>
2021-08-11 18:02:10 -0700

    Reorganized and refactored (#4)
    * added env var and changed gitignore
    
    * reorganized, refactored for abstractions
    
    * moved func from utils to db_handler

---
+++ algo_trading/schemas.py
@@ -0,0 +1,10 @@
+class DFColumns:
+    columns = {
+        "date": "text",
+        "open": "real",
+        "high": "real",
+        "low": "real",
+        "close": "real",
+        "adj_close": "real",
+        "volume": "bigint",
+    }
