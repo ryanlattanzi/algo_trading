1135a47fed13d61ecc29e81956eb7568de3055d7
Ryan Lattanzi <45903952+ryanlattanzi@users.noreply.github.com>
2021-08-26 10:47:43 -0400

    Rl/clean up db calculations (#8)
    * added env var and changed gitignore
    
    * reorganized imports
    
    * cleaned up sma calculations

---
+++ algo_trading/calculations.py
@@ -0,0 +1,9 @@
+from controllers import ColumnController
+
+
+class Calculator:
+    @staticmethod
+    def calculate_sma(df, rolling_col):
+        for col, val in ColumnController.sma_calculations().items():
+            df[col] = df[rolling_col].rolling(val).mean()
+        return df
