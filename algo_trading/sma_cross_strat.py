955685a955f38d442bd9fc8146827dc03463d4b6
ryanlattanzi <ryanlattanzi7@gmail.com>
2021-08-30 10:54:09 -0400

    massaged to make it work nice with simulation

--- algo_trading/sma_cross_strat.py
+++ algo_trading/sma_cross_strat.py
@@ -1,11 +1,11 @@
-from typing import Dict
+from typing import Tuple
 import os
-import sys
 import yaml as yl
 import pandas as pd
-import sqlalchemy as sa
-from db_handler import *
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
+
+from db_handler import get_db_handler
 from controllers import ColumnController
 
 
@@ -13,52 +13,46 @@ load_dotenv("../local.env")
 
 
 class SMACross:
+    def __init__(self, ticker: str, slice: pd.DataFrame, days_back: int = 6) -> None:
 
-    def __init__(self, ticker: str, db_info: Dict) -> None:
         self.ticker = ticker
-        self.db_info = db_info
+        self.slice = slice
+        self.days_back = days_back
 
-        self.db_engine = None
+        self._validate_slice()
 
-        self._initialize_db_connection()
+    def _validate_slice(self) -> None:
+        if len(self.slice) != self.days_back:
+            length = len(self.slice)
+            raise IndexError(
+                f"Given data slice has length {length}, but needs {self.days_back}"
+            )
 
+    def check_sma_cross(self) -> Tuple[datetime.date, str, str]:
 
-    def _initialize_db_connection(self) -> None:
-        user = self.db_info.get("user")
-        password = self.db_info.get("password")
-        database = self.db_info.get("database")
-        host = self.db_info.get("host")
-        port = self.db_info.get("port", "5432")
-
-        self.db_engine = sa.create_engine(
-            f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
-        )
-
-    def _get_table_slice(self):
-        condition = "ORDER BY date DESC LIMIT 6"
-        query = f"SELECT * FROM {self.ticker} {condition}".strip()
-        df = pd.read_sql(query, con=self.db_engine)
-        return df
-
-
-    def check_sma_cross(self):
-        df_slice = self._get_table_slice()
-
-        #buffer up should be greater than 1 and buffer down should be lower than 1 if you want to make breakout signals stronger
+        # buffer up should be greater than 1 and buffer down should be lower than 1 if you want to make breakout signals stronger
         buffer_up = 1
         buffer_down = 1
+        date = self.slice["date"].iloc[0]
 
-    # TODO: Need to figure out if the dataframe that I am analyzing is going to be in ascending or descending order. Currently it is in descending order when analyzing.
-        if df_slice['close'].iloc[0] <= df_slice ['ma_50'].iloc[0] or df_slice['ma_50'].iloc[0] <= df_slice['ma_200'].iloc[0]:
-            return 'BEAR MARKET'
+        # TODO: Need to figure out if the dataframe that I am analyzing is going to be in ascending or descending order. Currently it is in descending order when analyzing.
+        if (
+            self.slice["close"].iloc[0] <= self.slice["ma_50"].iloc[0]
+            or self.slice["ma_50"].iloc[0] <= self.slice["ma_200"].iloc[0]
+        ):
+            return (date, self.ticker, "BEAR MARKET")
         else:
-            if (buffer_up * (df_slice['ma_7'].iloc[0]) >= df_slice['ma_21'].iloc[0]) and (df_slice['ma_7'].iloc[1] < df_slice['ma_21'].iloc[1]):
-                return (df_slice['date'].iloc[0],self.ticker, 'BUY')
+            if (
+                buffer_up * (self.slice["ma_7"].iloc[0]) >= self.slice["ma_21"].iloc[0]
+            ) and (self.slice["ma_7"].iloc[1] < self.slice["ma_21"].iloc[1]):
+                return (date, self.ticker, "BUY")
 
-            elif (buffer_down * (df_slice['ma_7'].iloc[0]) < df_slice['ma_21'].iloc[0]) and (df_slice['ma_7'].iloc[1] >= df_slice['ma_21'].iloc[1]):
-                return (df_slice['date'].iloc[0],self.ticker, 'SELL')
+            elif (
+                buffer_down * (self.slice["ma_7"].iloc[0]) < self.slice["ma_21"].iloc[0]
+            ) and (self.slice["ma_7"].iloc[1] >= self.slice["ma_21"].iloc[1]):
+                return (date, self.ticker, "SELL")
             else:
-                return "HOLD"
+                return (date, self.ticker, "HOLD")
 
 
 if __name__ == "__main__":
@@ -70,7 +64,6 @@ if __name__ == "__main__":
 
     # Loading in and parsing CONFIG
     CONFIG = yl.safe_load(open("config.yml", "r"))
-    TICKERS = CONFIG["ticker_list"]
     DB_HANDLER = CONFIG["db_handler"]
     DATA_HANDLER = CONFIG["data_handler"]
 
@@ -84,6 +77,28 @@ if __name__ == "__main__":
         "port": "5432",
     }
 
-    #create aapl slice
-    aapl_class = SMACross('aapl', DB_INFO)
-    print(aapl_class.check_sma_cross())
+    # Creating db_handler (works with just one stock for now)
+    ticker = "aapl"
+    db_handler = get_db_handler(DB_HANDLER, [ticker], DB_INFO)
+
+    # Define a start pull date to run the simulation
+    start_date = "2021-08-01"
+    curr_date = datetime.strptime(start_date, DATE_FORMAT)
+
+    last_date_entry = db_handler.get_most_recent_date(ticker)
+
+    # Running SMA for every day since the given start date up until the most
+    # recent date in the DB
+    while curr_date <= datetime(
+        last_date_entry.year, last_date_entry.month, last_date_entry.day
+    ):
+        curr_date_str = curr_date.strftime(DATE_FORMAT)
+        days_back = 6
+        condition = (
+            f"WHERE date <= '{curr_date_str}' ORDER BY date DESC LIMIT {days_back}"
+        )
+        slice = db_handler.get_data(ticker, condition)
+        sma = SMACross(ticker, slice, days_back)
+        result = sma.check_sma_cross()
+        print(result)
+        curr_date = curr_date + timedelta(days=1)
