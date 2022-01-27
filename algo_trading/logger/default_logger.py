646736c17822495256c87d62a47ac0e5afefb283
Ryan Lattanzi <45903952+ryanlattanzi@users.noreply.github.com>
2022-01-27 10:04:14 -0500

    made config a little cleaner, not much though (#21)

--- algo_trading/logger/default_logger.py
+++ algo_trading/logger/default_logger.py
@@ -1,20 +1,33 @@
 import logging
 import sys
+from typing import Tuple
 
 from algo_trading.logger.controllers import LogLevelController, LogConfig
 
 
-def main_logger(config: LogConfig) -> logging.Logger:
+def get_main_logger(
+    log_name: str,
+    file_name: str,
+    log_level: LogLevelController,
+) -> Tuple[logging.Logger, LogConfig]:
     """Default settings for application logger. Automatically
     prints to stdout, but has the option to write to a file as well.
+    Returns logger and LogConfig needed for downstream modules.
 
     Args:
-        logger_name (str): Name of logger.
-        file_name (str, optional): File to output. Defaults to None.
+        log_name (str): Name of main logger.
+        file_name (str): File to write log.
+        log_level (LogLevelController): Log level.
 
     Returns:
-        logging.Logger: Logger object with default settings.
+        Tuple[logging.Logger, LogConfig]: Log object and LogConfig object.
     """
+
+    config = LogConfig(
+        log_name=log_name,
+        file_name=file_name,
+        log_level=log_level,
+    )
 
     _log_levels = {
         LogLevelController.info: logging.INFO,
@@ -39,8 +52,20 @@ def main_logger(config: LogConfig) -> logging.Logger:
         fh.setFormatter(formatter)
         logger.addHandler(fh)
 
-    return logger
+    return logger, config
 
 
-def child_logger(log_name: str, child_name: str):
+def get_child_logger(log_name: str, child_name: str) -> logging.Logger:
+    """Returns a child logger, which shows which module
+    the log message is coming from. This consolidates all
+    log messages into one file while still indicating
+    where the code was called from.
+
+    Args:
+        log_name (str): Name of main log.
+        child_name (str): Name of child logger.
+
+    Returns:
+        logging.Logger: Child log object.
+    """
     return logging.getLogger(log_name).getChild(child_name)
