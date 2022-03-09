08737e817f0a2bc860a1f9b48594b1358e66d2aa
ChrisMead <chrismead3.00@gmail.com>
2022-03-08 22:07:49 -0500

    working on macd

--- algo_trading/config/controllers.py
+++ algo_trading/config/controllers.py
@@ -16,6 +16,9 @@ class ColumnController(Enum):
     ma_50 = "ma_50"
     ma_21 = "ma_21"
     ma_7 = "ma_7"
+    ema_12 = "ema_12"
+    ema_26 = "ema_26"
+
 
     # Key Value stuff
     last_cross_up = "last_cross_up"
@@ -48,6 +51,8 @@ class ColumnController(Enum):
             cls.ma_50.value: "REAL",
             cls.ma_21.value: "REAL",
             cls.ma_7.value: "REAL",
+            cls.ema_12.value: "REAL",
+            cls.ema_26.value: "REAL",
         }
 
     @classmethod
@@ -59,6 +64,13 @@ class ColumnController(Enum):
             cls.ma_7.value: 7,
         }
 
+    @classmethod
+    def macd_calculations(cls) -> Dict:
+        return {
+            cls.ema_12.value: 12,
+            cls.ema_26.value: 26,
+            cls.macd.value: 
+        }
 
 class StockStatusController(Enum):
     buy = "BUY"
--- algo_trading/utils/calculations.py
+++ algo_trading/utils/calculations.py
@@ -7,3 +7,13 @@ class Calculator:
         for col, val in ColumnController.sma_calculations().items():
             df[col] = df[rolling_col].rolling(val).mean()
         return df
+    
+    @staticmethod
+    def calculate_ema(df,rolling_col):
+        for col, val in ColumnController.macd_calculations().items():
+            df[col] = df[rolling_col].ewm(span=val,adjust=False, min_periods=val).mean()
+    
+    @staticmethod
+    def calculate_macd(df,rolling_col):
+        for col,val in ColumnController.macd_calculations().items():
+            df[col] = 
