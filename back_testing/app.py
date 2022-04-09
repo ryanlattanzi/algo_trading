4911059889d6008baf2f9afd8c15fca551cbc801
Ryan Lattanzi <45903952+ryanlattanzi@users.noreply.github.com>
2022-04-09 19:02:14 -0400

    SMACrossBackTester Unit Tests (#42)
    * modified sma backtester behavior
    
    * backtester unit tests and data
    
    * trade_book type hint
    
    * deleted auto import
    
    * separate injection

--- back_testing/app.py
+++ back_testing/app.py
@@ -1,14 +1,20 @@
 from typing import Dict
 from fastapi import FastAPI
 
+from algo_trading.logger.default_logger import get_main_logger
+from algo_trading.logger.controllers import LogLevelController
+
 from src.sma_cross_backtest import SMACrossBackTester
-from src.controllers import BackTestPayload, BackTestOptions, BackTestResult
+from src.controllers import BackTestPayload, BackTestOptions
+from src.config import DB_INFO, DB_HANDLER
 
 app = FastAPI()
 
-STRATEGIES = {
-    BackTestOptions.sma_cross: SMACrossBackTester,
-}
+LOG, LOG_INFO = get_main_logger(
+    log_name="SMA_backtest",
+    file_name=None,
+    log_level=LogLevelController.info,
+)
 
 
 @app.post("/backtest")
@@ -24,8 +30,18 @@ def backtest(payload: BackTestPayload) -> Dict:
         Dict: Status
     """
 
-    tester = STRATEGIES[payload.strategy](payload=payload)
-    res: BackTestResult = tester.test()
+    strategies = {
+        BackTestOptions.sma_cross: SMACrossBackTester(
+            payload=payload,
+            db_info=DB_INFO,
+            db_handler=DB_HANDLER,
+            log=LOG,
+            log_info=LOG_INFO,
+        ),
+    }
+
+    tester = strategies[payload.strategy]
+    res, _ = tester.test()
 
     return {
         "status": 200,
